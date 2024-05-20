6ac631076a10d74ee27d8d4cd8264f59106235d2
nhuynh011 <114946408+nhuynh011@users.noreply.github.com>
2024-05-20 14:04:37 -0400

    First upload May 20th. Binded all datasets for preCOVID and postCOVID together. Starting analysis with both

---
+++ cleaning.R
@@ -0,0 +1,120 @@
+# Bare script for file cleaning. Will be put into function format another time. 
+#Need to setwd (do later)
+
+# PRE-CLEANING
+#Load libraries for cleaning:
+library(tidyverse)
+library(readxl)
+library(here)
+
+#Load file 
+df <- read_xlsx(here("raw", "SURVEY FOR MA231-02 in Fall 2022.xlsx"))
+
+#Remove columns A to K (the first question should be favorite season)
+df <- df[ -c(1:11) ]
+
+#Remove rows (first row should be row 4)
+df <- df[-c(1:2), ]
+
+#Rename columns:
+names(df) <- c("season", "iphone", "windows", "mndegrees", "calc1", "apcalc", "gradecalc2", "gradediffeq", "calc2", "calc3", "diffeq", "nocourses", "gradecalc3", "screentime", "studyhours", "difficulty", "coursepref", "inpersonperf")
+
+
+# Data dictionary:
+# mndegrees = number of minor degrees student is pursuing
+# calc1 = did the student attend calculus 1 (MA131) at Clarkson University?
+# nocourses = did the student not attend calculus 2 and calculus 3 and differential equations at Clarkson?
+# coursepref = does the student prefer the course to be in person or online?
+# inpersonperf = which format (online or in person) does the student feel they perform better in?
+
+# CLEANING
+#Changing responses in columns that need 0 and 1:
+#If we need to replace NA:
+# df[is.na(df)] <- 0
+
+#iPhone (keep NA as NA):
+df$iphone<-replace(df$iphone, df$iphone == "Android based phone", 0)
+df$iphone<-replace(df$iphone, df$iphone == "iPhone", 1)
+
+#Windows (keep NA as NA):
+df$windows<-replace(df$windows, df$windows == "Mac OS", 0)
+df$windows<-replace(df$windows, df$windows == "Windows", 1)
+
+#Calc1 (keep NA as NA):
+df$calc1<-replace(df$calc1, df$calc1 == "No", 0)
+df$calc1<-replace(df$calc1, df$calc1 == "Yes", 1)
+
+#APCalc (keep NA as NA):
+df$apcalc<-replace(df$apcalc, df$apcalc == "No", 0)
+df$apcalc<-replace(df$apcalc, df$apcalc == "Yes", 1)
+
+#Calc2 (NA = 0):
+df$calc2[is.na(df$calc2)] <- 0
+df$calc2<-replace(df$calc2, df$calc2 != 0, 1)
+
+#Calc3 (NA = 0):
+df$calc3[is.na(df$calc3)] <- 0
+df$calc3<-replace(df$calc3, df$calc3 != 0, 1)
+
+#DiffEq (NA = 0):
+df$diffeq[is.na(df$diffeq)] <- 0
+df$diffeq<-replace(df$diffeq, df$diffeq != 0, 1)
+
+#NoCourses (NA = 0):
+df$nocourses[is.na(df$nocourses)] <- 0
+df$nocourses<-replace(df$nocourses, df$nocourses != 0, 1)
+
+#CoursePref (in person = 1, else 0, keep NA as NA):
+df$coursepref<-replace(df$coursepref, df$coursepref == "In-person", 1)
+df$coursepref<-replace(df$coursepref, df$coursepref == "Online", 0)
+
+#InPersonPerf (in person = 1, online = 0, keep NA as NA):
+df$inpersonperf<-replace(df$inpersonperf, df$inpersonperf == "Online format", 0)
+df$inpersonperf<-replace(df$inpersonperf, df$inpersonperf == "In-person format", 1)
+
+# Converting responses from a categorical to numeric:
+#GradeCalc2 (0 to 12):
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "A+", 12)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "A", 12)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "A-", 11)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "B+", 10)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "B", 9)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "B-", 8)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "C+", 7)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "C", 6)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "C-", 5)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "D", 3)
+df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "F", 0)
+
+#GradeDiffEq (0 to 12):
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A+", 12)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A", 12)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A-", 11)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B+", 10)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B", 9)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B-", 8)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C+", 7)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C", 6)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C-", 5)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "D", 3)
+df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "F", 0)
+
+#Difficulty (More = 1, Less = -1, else = 0, keep NA as NA):
+df$difficulty<-replace(df$difficulty, df$difficulty == "more than other courses", 1)
+df$difficulty<-replace(df$difficulty, df$difficulty == "about the same as other courses", 0)
+df$difficulty<-replace(df$difficulty, df$difficulty == "less than other courses", -1)
+
+# Format as numeric to help excel export:
+for (x in 2:18) {
+  df[x] <- as.numeric(unlist(df[x]))
+}
+
+# Format some things by hand after
+
+# Write out as an xlsx file??
+library(openxlsx)
+write.xlsx(df, "MA231 ONE PM FALL 2022.xlsx")
+
+# Then turn this into a function(?)
+
+
---
+++ prelim.R
@@ -0,0 +1,89 @@
+#PRELIM OBSERVATIONS
+# What questions can we ask with this data?
+
+# Load lib
+library(tidyverse)
+library(here)
+library(chron)
+library(readxl)
+library(dplyr)
+
+# Load files (5)
+c1<- read_xlsx(here("MA231 TEN AM FALL 2022.xlsx"))
+c2<- read_xlsx(here("MA231 ONE PM FALL 2022.xlsx"))
+d1<- read_xlsx(here("MA232 NINE AM SPRING 2024.xlsx"))
+d2<- read_xlsx(here("MA232 TWELVE PM SPRING 2024.xlsx"))
+s<- read_xlsx(here("MA232 TWELVE PM SPRING 2023.xlsx"))
+
+# Add more information to the files and combine them
+# Add column describing the year, course name, and semester
+c1 <- c1|> mutate(year = 22,
+                  semester = 0, #fall = 0, spring = 1
+                  time = times(paste0(10, ":00", ":00")),
+                  coursecode = 231)
+c2 <- c2|> mutate(year = 22,
+                  semester = 0, #fall = 0, spring = 1
+                  time = times(paste0(13, ":00", ":00")),
+                  coursecode = 231) 
+d1 <- d1|> mutate(year = 24,
+                  semester = 1, #fall = 0, spring = 1
+                  time = times(paste0(9, ":00", ":00")),
+                  coursecode = 232,) 
+d2 <- d2|> mutate(year = 24,
+                  semester = 1, #fall = 0, spring = 1
+                  time = times(paste0(12, ":00", ":00")),
+                  coursecode = 232) 
+s <- s|> mutate(year = 23,
+                semester = 1, #fall = 0, spring =1
+                time = times(paste0(12, ":00", ":00")),
+                coursecode = 232) 
+
+# Re-arrange everything
+c1 <- c1|>relocate(c("year", "semester", "time", "coursecode"))
+c2 <- c2|>relocate(c("year", "semester", "time", "coursecode"))
+d1 <- d1|>relocate(c("year", "semester", "time", "coursecode"))
+d2 <- d2|>relocate(c("year", "semester", "time", "coursecode"))
+s <- s|>relocate(c("year", "semester", "time", "coursecode"))
+
+# Combine sets
+calc3total <- rbind(c1, c2)
+diffeqtotal <- rbind(d1, d2, s)
+diffeqdf <- rbind(d1, d2)
+
+# Move stuff around
+calc3total <- calc3total|>relocate("gradecalc2", .after = "noprereq")
+diffeqtotal <- diffeqtotal|>relocate("gradecalc2", .after = "noprereq")
+
+# bind all
+cols3 <- colnames(select(calc3total, -c("gradediffeq", "diffeq", "noprereq", "gradecalc3")))
+postCOVID <- rbind(
+  subset(calc3total, select = cols3), 
+  subset(diffeqtotal, select = cols3)
+)
+
+# Meta analysis of past data, load data
+preCOVID <- read_xlsx("datafor_r.xlsx")
+
+# Clean
+# time here is if the class is MWF or TuTh, where MWF is 1.
+names(preCOVID) <- c("aem", "calc1", "calc2", "calc3", "diffeq", "gpapr", "gradyr", "iphone", "time", "mjdegrees", "mndegrees", "NUniPre", "PhAlwys", "PhAwy", "PhDistr", "PhOnce", "screentime")
+preCOVID <- past|> mutate(year = 18,
+                  semester = 0, #fall = 0, spring = 1
+                  coursecode = 330)
+preCOVID <- preCOVID|>relocate(c("year", "semester", "time", "coursecode"))
+
+# Now do some general graphs
+ggplot(calc3total, aes(x = factor(time), y = iphone)) + 
+  geom_bar(stat = "summary", fun = "mean")
+ggplot(calc3total, aes(x = factor(time), y = gradecalc2)) + 
+  geom_bar(stat = "summary", fun = "mean")
+ggplot(calc3total, aes(x = factor(time), y = studyhours)) + 
+  geom_bar(stat = "summary", fun = "mean")
+ggplot(calc3total, aes(x = factor(time), y = windows)) + 
+  geom_bar(stat = "summary", fun = "mean")
+
+# Be sure to update everything into the drive
+# Unload all files into the drive so things there can run if you download anything.
+
+# t-test, anova, two population proportion test
+# moving towards understanding our analysis and making sense of what we are doing.
