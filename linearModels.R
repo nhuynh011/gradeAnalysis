c10fa6b339467c9be7f167dad4936cbc4a39e7a8
Phong Huynh <mihuynh@clarkson.edu>
2024-07-07 17:44:54 -0700

    Cleaned code and consistently removed outliers.

--- linearModels.R
+++ linearModels.R
@@ -18,10 +18,7 @@ diffeq <- diffeq[, !names(diffeq) %in% "noprereq"]
 calcrm<- na.omit(calc3[-c(1:5, 20:21)])
 diffrm<- na.omit(diffeq[-c(1:5, 18:19)])
 
-nrow(calcrm) #need 5 x variables to have enough to do analysis
-nrow(diffrm)
-
-### Correlation coeff
+### Correlation coeff, warnings are na that haven't been removed yet
 library(GGally)
 # AEM is the response
 ggpairs(aem)
@@ -31,38 +28,38 @@ ggpairs(calc3[c(17, 5:16, 18:21)])
 # gradediffeq is the response
 ggpairs(diffeq[c(15, 5:14, 16:19)])
 
-### Making the stepwise models:
+################################################################################
+### Making the stepwise models (with outliers):
 # Calc3
-# Forward model:
 forward <- lm(gradecalc3 ~ 1, data = calcrm)
-forward <- step(forward, direction = "forward", scope = formula(lm(gradecalc3 ~ ., data = calcrm)))
-summary(forward)
-#Backwards model:
-back <- lm(gradecalc3 ~ ., data = calcrm)
-back <- step(back, direction = "backward", trace = 0)
-summary(back)
+calc3model <- step(forward, direction = "forward", scope = formula(lm(gradecalc3 ~ ., data = calcrm)))
+summary(calc3model)
 
-###
-# DiffEq
-# Forward model:
-forward <- lm(gradediffeq ~ 1, data = diffrm)
-forwardlm <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diffrm)))
-summary(forwardlm)
-#Backwards model:
-back <- lm(gradediffeq ~ ., data = na.omit(diffrm))
+back <- lm(gradecalc3 ~ ., data = calcrm)
 backlm <- step(back, direction = "backward", trace = 0)
 summary(backlm)
 
-# Back+forward step model:
 both <- lm(gradecalc3 ~ ., data = calcrm)
 summary(both)
-calc3model <- step(both, direction = "both", trace = 0)
-summary(calc3model)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed that all 3 models are the same
+
+###
+# DiffEq
+forward <- lm(gradediffeq ~ 1, data = diffrm)
+diffeqmodel <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diffrm)))
+summary(diffeqmodel)
+
+back <- lm(gradediffeq ~ ., data = diffrm)
+backlm <- step(back, direction = "backward", trace = 0)
+summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diffrm)
 summary(both)
-diffeqmodel <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed that all 3 models are the same
 
 ################################################################################
 ### MAKING THE STEPWISE MODEL AFTER REMOVING OUTLIERS
@@ -75,29 +72,28 @@ iqr <- IQR(diffrm$screentime, na.rm = TRUE)
 diffrm <- subset(diffrm, diffrm$screentime > (Q[1] - 1.5*iqr) & diffrm$screentime < (Q[2]+1.5*iqr)) #save over original
 # Original was 0 to 18
 
-# minor degrees
-#nothing to remove for diffeq here
-Q <- quantile(diffrm$mndegrees, probs=c(.25, .75), na.rm = TRUE)
-iqr <- IQR(diffrm$mndegrees, na.rm = TRUE)
-diffrm <- subset(diffrm, diffrm$mndegrees > (Q[1] - 1.5*iqr) & diffrm$mndegrees < (Q[2]+1.5*iqr)) #save over original
+# study hours
+Q <- quantile(diffrm$studyhours, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diffrm$studyhours, na.rm = TRUE)
+diffrm <- subset(diffrm, diffrm$studyhours > (Q[1] - 1.5*iqr) & diffrm$studyhours < (Q[2]+1.5*iqr)) #save
 
 nrow(diffrm) #-11 outliers from screentime (range is 2 to 8 hours)
 #-20 from study hours (range is 1 to 4)
 
-# Do all 3 models to compare:
-#Diffeq model no funny study hours
+#Diffeq model
 forward <- lm(gradediffeq ~ 1, data = diffrm)
-forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diffrm)))
-summary(forward)
+diffeqrmmodel <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diffrm)))
+summary(diffeqrmmodel)
 
 back <- lm(gradediffeq ~ ., data = diffrm)
 backlm <- step(back, direction = "backward", trace = 0)
 summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diffrm)
-# summary(both)
-diffeqmodel <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel)
+summary(both)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed all are the same
 
 ### CALC3
 # Lm without outliers
@@ -107,53 +103,71 @@ summary(diffeqmodel)
 Q <- quantile(calcrm$screentime, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(calcrm$screentime, na.rm = TRUE)
 calcrm <- subset(calcrm, calcrm$screentime > (Q[1] - 1.5*iqr) & calcrm$screentime < (Q[2]+1.5*iqr)) #save over original
-#63 rows, 2 hours to 10 hours yikes...
-#original was 2 to 21
-
-# minor degrees
-# None removed here
-Q <- quantile(calcrm$mndegrees, probs=c(.25, .75), na.rm = TRUE)
-iqr <- IQR(calcrm$mndegrees, na.rm = TRUE)
-calcrm <- subset(calcrm, calcrm$mndegrees > (Q[1] - 1.5*iqr) & calcrm$mndegrees < (Q[2]+1.5*iqr)) #save over original
-
 
 # study hours
 Q <- quantile(calcrm$studyhours, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(calcrm$studyhours, na.rm = TRUE)
 calcrm <- subset(calcrm, calcrm$studyhours > (Q[1] - 1.5*iqr) & calcrm$studyhours < (Q[2]+1.5*iqr)) #save
-nrow(calcrm) #removed 2, range is 0 to 9 hours
-# original was 0 to 15
+nrow(calcrm)
 
-# Do models to compare:
-#aem model (2018)
-aem <- na.omit(aem[-c(6, 9, 13:16)])
-forward <- lm(AEM ~ 1, data = aem)
-aemmodel2018 <- step(forward, direction = "forward", scope = formula(lm(AEM ~ ., data = aem)))
-summary(aemmodel2018)
-
-#Calc3 model (2022)
+#Calc3 model
 forward <- lm(gradecalc3 ~ 1, data = calcrm)
-forward <- step(forward, direction = "forward", scope = formula(lm(gradecalc3 ~ ., data = calcrm)))
-summary(forward)
+calc3model2022 <- step(forward, direction = "forward", scope = formula(lm(gradecalc3 ~ ., data = calcrm)))
+summary(calc3model2022)
 
 back <- lm(gradecalc3 ~ ., data = calcrm)
 backlm <- step(back, direction = "backward", trace = 0)
 summary(backlm)
 
 both <- lm(gradecalc3 ~ ., data = calcrm)
-# summary(both)
-calc3model22 <- step(both, direction = "both", trace = 0)
-summary(calc3model22)
+summary(both)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed all are the same
 
-### Diffeq model by years:
-#2023, decided not to remove outliers
+### AEM
+# Do models to compare:
+#aem model (2018)
+aem <- na.omit(aem[-c(6, 9, 13:16)])
+
+forward <- lm(AEM ~ 1, data = aem)
+aemmodel2018 <- step(forward, direction = "forward", scope = formula(lm(AEM ~ ., data = aem)))
+summary(aemmodel2018)
+
+back <- lm(AEM ~ ., data = aem)
+backlm <- step(back, direction = "backward", trace = 0)
+summary(backlm)
+
+both <- lm(AEM ~ ., data = aem)
+summary(both)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed all same
+
+################################################################################
+# Models by year
+### Calc3 model (2022)
+# Already done above, called calc3model2022
+
+### Diffeq model (2023)
 diff2023 <- diffeq |> filter(year == "23") #50 rows
 diff2023 <- na.omit(diff2023[-c(1:5, 18:19)])
 
-### Linear model
+# Remove outliers
+#Study hours
+Q <- quantile(diff2023$studyhours, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff2023$studyhours, na.rm = TRUE)
+diff2023 <- subset(diff2023, diff2023$studyhours > (Q[1] - 1.5*iqr) & diff2023$studyhours < (Q[2]+1.5*iqr)) #save over original
+
+#Screentime
+Q <- quantile(diff2023$screentime, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff2023$screentime, na.rm = TRUE)
+diff2023 <- subset(diff2023, diff2023$screentime > (Q[1] - 1.5*iqr) & diff2023$screentime < (Q[2]+1.5*iqr)) #save over original
+
+### Linear model (2023)
 forward <- lm(gradediffeq ~ 1, data = diff2023)
-forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff2023)))
-summary(forward)
+diffeqmodel23 <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff2023)))
+summary(diffeqmodel23)
 
 back <- lm(gradediffeq ~ ., data = diff2023)
 backlm <- step(back, direction = "backward", trace = 0)
@@ -161,21 +175,29 @@ summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diff2023)
 summary(both)
-diffeqmodel23 <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel23)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed all the same
 
-# 2024, removed outliers
+### Diffeq model (2024)
 diff2024 <- diffeq |> filter(year == "24")
 diff2024 <- na.omit(diff2024[-c(1:5, 18:19)])
 
-# Remove screentime outliers:
+# Remove outliers
+# Study hours:
+Q <- quantile(diff2024$studyhours, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff2024$studyhours, na.rm = TRUE)
+diff2024 <- subset(diff2024, diff2024$studyhours > (Q[1] - 1.5*iqr) & diff2024$studyhours < (Q[2]+1.5*iqr)) #save over original
+
+# screentime:
 Q <- quantile(diff2024$screentime, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(diff2024$screentime, na.rm = TRUE)
 diff2024 <- subset(diff2024, diff2024$screentime > (Q[1] - 1.5*iqr) & diff2024$screentime < (Q[2]+1.5*iqr)) #save over original
 
+### Linear model (2024)
 forward <- lm(gradediffeq ~ 1, data = diff2024)
-forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff2024)))
-summary(forward)
+diffeqmodel24 <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff2024)))
+summary(diffeqmodel24)
 
 back <- lm(gradediffeq ~ ., data = diff2024)
 backlm <- step(back, direction = "backward", trace = 0)
@@ -183,10 +205,11 @@ summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diff2024)
 summary(both)
-diffeqmodel24 <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel24)
+bothlm <- step(both, direction = "both", trace = 0)
+summary(bothlm)
+# confirmed all are same
 
-###
+### Do each of the sections give a different result?
 # Split DIFFEQ ANALYSIS
 # Make a different model for each of the semesters (applies to diffeq only)
 diff1 <- diffeq|> filter(year == 23)
@@ -203,7 +226,7 @@ ggpairs(diff1)
 ggpairs(diff2)
 ggpairs(diff3)
 
-#Diff1 Model:
+# Diff1 Model (12PM Spring 2023):
 forward <- lm(gradediffeq ~ 1, data = diff1)
 forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff1)))
 summary(forward)
@@ -214,10 +237,35 @@ summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diff1)
 summary(both)
-diffeqmodel <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel)
+diffeq1model <- step(both, direction = "both", trace = 0)
+summary(diffeq1model)
+# confirmed all models are the same
 
-#Diff2 Model:
+# Diff1 model after removing both sets of outliers:
+# study hours
+Q <- quantile(diff1$studyhours, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff1$studyhours, na.rm = TRUE)
+diff1 <- subset(diff1, diff1$studyhours > (Q[1] - 1.5*iqr) & diff1$studyhours < (Q[2]+1.5*iqr)) #save over original
+
+# screentime
+Q <- quantile(diff1$screentime, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff1$screentime, na.rm = TRUE)
+diff1 <- subset(diff1, diff1$screentime > (Q[1] - 1.5*iqr) & diff1$screentime < (Q[2]+1.5*iqr)) #save over original
+
+forward <- lm(gradediffeq ~ 1, data = diff1)
+forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff1)))
+summary(forward)
+
+back <- lm(gradediffeq ~ ., data = diff1)
+backlm <- step(back, direction = "backward", trace = 0)
+summary(backlm)
+
+both <- lm(gradediffeq ~ ., data = diff1)
+summary(both)
+diffeq1rmmodel <- step(both, direction = "both", trace = 0)
+summary(diffeq1rmmodel)
+
+# Diff2 Model (9AM Spring 2024):
 forward <- lm(gradediffeq ~ 1, data = diff2)
 forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff2)))
 summary(forward)
@@ -228,10 +276,35 @@ summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diff2)
 summary(both)
-diffeqmodel <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel)
+diffeq2model <- step(both, direction = "both", trace = 0)
+summary(diffeq2model)
+# all are the same
 
-#Diff3 Model:
+# Diff2 model after removing both sets of outliers:
+# study hours
+Q <- quantile(diff2$studyhours, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff2$studyhours, na.rm = TRUE)
+diff2 <- subset(diff2, diff2$studyhours > (Q[1] - 1.5*iqr) & diff2$studyhours < (Q[2]+1.5*iqr)) #save over original
+
+# screentime
+Q <- quantile(diff2$screentime, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff2$screentime, na.rm = TRUE)
+diff2 <- subset(diff2, diff2$screentime > (Q[1] - 1.5*iqr) & diff2$screentime < (Q[2]+1.5*iqr)) #save over original
+
+forward <- lm(gradediffeq ~ 1, data = diff2)
+forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff2)))
+summary(forward)
+
+back <- lm(gradediffeq ~ ., data = diff2)
+backlm <- step(back, direction = "backward", trace = 0)
+summary(backlm)
+
+both <- lm(gradediffeq ~ ., data = diff2)
+summary(both)
+diffeq2rmmodel <- step(both, direction = "both", trace = 0)
+summary(diffeq2rmmodel)
+
+# Diff3 Model (12PM Spring 2024):
 forward <- lm(gradediffeq ~ 1, data = diff3)
 forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff3)))
 summary(forward)
@@ -242,5 +315,30 @@ summary(backlm)
 
 both <- lm(gradediffeq ~ ., data = diff3)
 summary(both)
-diffeqmodel <- step(both, direction = "both", trace = 0)
-summary(diffeqmodel)
+diffeq3model <- step(both, direction = "both", trace = 0)
+summary(diffeq3model)
+# all are the same
+
+# Diff3 model after removing both sets of outliers:
+# study hours
+Q <- quantile(diff3$studyhours, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff3$studyhours, na.rm = TRUE)
+diff3 <- subset(diff3, diff3$studyhours > (Q[1] - 1.5*iqr) & diff3$studyhours < (Q[2]+1.5*iqr)) #save over original
+
+# screentime
+Q <- quantile(diff3$screentime, probs=c(.25, .75), na.rm = TRUE)
+iqr <- IQR(diff3$screentime, na.rm = TRUE)
+diff3 <- subset(diff3, diff3$screentime > (Q[1] - 1.5*iqr) & diff3$screentime < (Q[2]+1.5*iqr)) #save over original
+
+forward <- lm(gradediffeq ~ 1, data = diff3)
+forward <- step(forward, direction = "forward", scope = formula(lm(gradediffeq ~ ., data = diff3)))
+summary(forward)
+
+back <- lm(gradediffeq ~ ., data = diff3)
+backlm <- step(back, direction = "backward", trace = 0)
+summary(backlm)
+
+both <- lm(gradediffeq ~ ., data = diff3)
+summary(both)
+diffeq3rmmodel <- step(both, direction = "both", trace = 0)
+summary(diffeq3rmmodel)
