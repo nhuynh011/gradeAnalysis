9e3b64d3873750c0c3e878e17a42ffe31dc73d4e
Phong Huynh <mihuynh@clarkson.edu>
2024-07-07 18:31:33 -0700

    Fixed file name inconsistencies and removed uneeded test.

--- tTest.R
+++ tTest.R
@@ -18,16 +18,16 @@ diff2 <- diffeq|> filter(time == "09:00:00")
 diff3 <- diffeq|> filter(time == "12:00:00", year == 24)
 
 # Remove na and correct columns
-diff1<- na.omit(diff1[-c(1:5, 18:19)])
-diff2<- na.omit(diff2[-c(1:5, 18:19)])
-diff3<- na.omit(diff3[-c(1:5, 18:19)])
+diff1<- na.omit(diff1[-c(2:5, 18:19)])
+diff2<- na.omit(diff2[-c(2:5, 18:19)])
+diff3<- na.omit(diff3[-c(2:5, 18:19)])
 
 # Add more information for diffeq
 diff1<- diff1 |> mutate(section = "12PM Spring 2023")
 diff2<- diff2 |> mutate(section = "9AM Spring 2024")
 diff3<- diff3 |> mutate(section = "12PM Spring 2024")
 
-difftotal <- rbind(diff1, diff2, diff3)
+diffeqtotal <- rbind(diff1, diff2, diff3)
 
 ################################################################################
 ### Screentime
@@ -41,20 +41,6 @@ library(ggstatsplot)
 Q <- quantile(aemCalc1$screentime, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(aemCalc1$screentime, na.rm = TRUE)
 aemCalc <- subset(aemCalc1, aemCalc1$screentime > (Q[1] - 1.5*iqr) & aemCalc1$screentime < (Q[2]+1.5*iqr))
-t.test(screentime ~ as.factor(year), data = aemCalc)
-
-# 2018 and 2023
-aemCalc2 <- all|>filter(year == 18 | year == 23) 
-Q <- quantile(aemCalc2$screentime, probs=c(.25, .75), na.rm = TRUE)
-iqr <- IQR(aemCalc2$screentime, na.rm = TRUE)
-aemCalc <- subset(aemCalc2, aemCalc2$screentime > (Q[1] - 1.5*iqr) & aemCalc2$screentime < (Q[2]+1.5*iqr))
-t.test(screentime ~ as.factor(year), data = aemCalc)
-
-# 2018 and 2024
-aemCalc3 <- all|>filter(year == 18 | year == 24) 
-Q <- quantile(aemCalc3$screentime, probs=c(.25, .75), na.rm = TRUE)
-iqr <- IQR(aemCalc3$screentime, na.rm = TRUE)
-aemCalc <- subset(aemCalc3, aemCalc3$screentime > (Q[1] - 1.5*iqr) & aemCalc3$screentime < (Q[2]+1.5*iqr))
 t.test(screentime ~ as.factor(year), data = aemCalc)
 
 # 2022 to 2023
@@ -132,21 +118,21 @@ Diff <- subset(diffeqtotal, diffeqtotal$gradediffeq > (Q[1] - 1.5*iqr) & diffeqt
 t.test(gradediffeq ~ as.factor(year), data = Diff)
 
 # 2023 section 1 to 2024 section 2 at 9am
-diff12 <- difftotal|>filter(section == "12PM Spring 2023" | section == "9AM Spring 2024")
+diff12 <- diffeqtotal|>filter(section == "12PM Spring 2023" | section == "9AM Spring 2024")
 Q <- quantile(diff12$gradediffeq, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(diff12$gradediffeq, na.rm = TRUE)
 Diff <- subset(diff12, diff12$gradediffeq > (Q[1] - 1.5*iqr) & diff12$gradediffeq < (Q[2]+1.5*iqr))
 t.test(gradediffeq ~ as.factor(section), data = Diff)
 
 # 2023 section 1 to 2024 section 3 at 12PM
-diff13 <- difftotal|>filter(section == "12PM Spring 2023" | section == "12PM Spring 2024")
+diff13 <- diffeqtotal|>filter(section == "12PM Spring 2023" | section == "12PM Spring 2024")
 Q <- quantile(diff13$gradediffeq, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(diff13$gradediffeq, na.rm = TRUE)
 Diff <- subset(diff13, diff13$gradediffeq > (Q[1] - 1.5*iqr) & diff13$gradediffeq < (Q[2]+1.5*iqr))
 t.test(gradediffeq ~ as.factor(section), data = Diff)
 
 # 2024 sections
-diff23 <- difftotal|>filter(section == "9AM Spring 2023" | section == "12PM Spring 2024")
+diff23 <- diffeqtotal|>filter(section == "9AM Spring 2024" | section == "12PM Spring 2024")
 Q <- quantile(diff23$gradediffeq, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(diff23$gradediffeq, na.rm = TRUE)
 Diff <- subset(diff23, diff23$gradediffeq > (Q[1] - 1.5*iqr) & diff23$gradediffeq < (Q[2]+1.5*iqr))
@@ -154,11 +140,11 @@ t.test(gradediffeq ~ as.factor(section), data = Diff)
 
 # Study hours
 # Testing for difference in study hours. Observed in the bar charts earlier in prelim.R
-calc3 <- read_xlsx(here("calc3.xlsx"))
-calc3 <- calc3[, !names(calc3) %in% "noprereq"]
+calc3total <- read_xlsx(here("calc3.xlsx"))
+calc3total <- calc3total[, !names(calc3total) %in% "noprereq"]
 
 # Bind with diffeq
-cols3 <- colnames(select(calc3total, -c("gradediffeq", "diffeq", "noprereq", "gradecalc3")))
+cols3 <- colnames(dplyr::select(diffeqtotal, -c("gradediffeq", "section")))
 postCOVID <- rbind(
   subset(calc3total, select = cols3), 
   subset(diffeqtotal, select = cols3)
@@ -185,3 +171,4 @@ Q <- quantile(post3$studyhours, probs=c(.25, .75), na.rm = TRUE)
 iqr <- IQR(post3$studyhours, na.rm = TRUE)
 post <- subset(post3, post3$studyhours > (Q[1] - 1.5*iqr) & post3$studyhours < (Q[2]+1.5*iqr))
 t.test(studyhours ~ as.factor(year), data = post)
+
