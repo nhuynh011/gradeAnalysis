8f32261b6d2c20ba1a5538845e880db963d3f7ed
nhuynh011 <mihuynh@clarkson.edu>
2024-08-19 09:55:41 -0700

    Updated file format

--- cleaning.R
+++ cleaning.R
@@ -1,6 +1,6 @@
 ### Cleaning file
 # Only cleans 1 file at a time
-# Currently set for cleaning MA231 files only, read comments to see which one is needed for diffeq/calc3
+# Currently set for cleaning MA232 (DiffEq) files only, read comments to see which one is needed for diffeq/calc3
 
 # PRE-CLEANING
 # Load libraries for cleaning:
@@ -9,20 +9,19 @@ library(readxl)
 library(here)
 
 # Load file 
-df <- read_xlsx(here("raw", "SURVEY FOR MA231-01 in Fall 2022.xlsx"))
+df <- read_xlsx(here("raw", "SURVEY FOR MA232 at noon- Spring 2024.xlsx"))
 
 #Remove columns A to K (the first question should be favorite season)
 df <- df[ -c(1:11) ]
 
-#Remove rows (first row should be row 4)
-df <- df[-c(1:2), ]
-
 # Rename columns:
 # MA231 (Calc 3)
-names(df) <- c("season", "iphone", "windows", "mndegrees", "calc1", "apcalc", "gradecalc2", "gradediffeq", "calc2", "calc3", "diffeq", "noprereq", "gradecalc3", "screentime", "studyhours", "difficulty", "coursepref", "inpersonperf")
+#names(df) <- c("season", "iphone", "windows", "mndegrees", "calc1", "apcalc", "gradecalc2", "gradediffeq", "calc2", "calc3", "diffeq", "noprereq", "gradecalc3", "screentime", "studyhours", "difficulty", "coursepref", "inpersonperf")
 # MA232 (Diff Eq)
-#names(df) <- c("season", "iphone", "windows", "mndegrees", "calc1", "apcalc", "gradecalc2", "calc2", "calc3", "noprereq", "gradediffeq", "screentime", "studyhours", "difficulty", "coursepref", "inpersonperf")
+names(df) <- c("season", "iphone", "windows", "mndegrees", "calc1", "apcalc", "gradecalc2", "calc2", "calc3", "noprereq", "gradediffeq", "screentime", "studyhours", "difficulty", "coursepref", "inpersonperf")
 
+# Delete description row (now that we have column names, we don't need this)
+df <- df[-1,]
 
 # Data dictionary:
 # mndegrees = number of minor degrees student is pursuing
@@ -61,8 +60,8 @@ df$calc3[is.na(df$calc3)] <- 0
 df$calc3<-replace(df$calc3, df$calc3 != 0, 1)
 
 #DiffEq (NA = 0): (Only for Calulus 3)
-df$diffeq[is.na(df$diffeq)] <- 0
-df$diffeq<-replace(df$diffeq, df$diffeq != 0, 1)
+#df$diffeq[is.na(df$diffeq)] <- 0
+#df$diffeq<-replace(df$diffeq, df$diffeq != 0, 1)
 
 #NoPrereq(uisites) (NA = 0):
 df$noprereq[is.na(df$noprereq)] <- 0
@@ -91,25 +90,29 @@ df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "D", 3)
 df$gradecalc2<-replace(df$gradecalc2, df$gradecalc2 == "F", 0)
 
 #GradeDiffEq (0 to 12), only use for MA231 (Calculus 3):
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A+", 13)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A", 12)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A-", 11)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B+", 10)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B", 9)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B-", 8)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C+", 7)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C", 6)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C-", 5)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "D", 3)
-df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "F", 0)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A+", 13)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A", 12)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "A-", 11)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B+", 10)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B", 9)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "B-", 8)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C+", 7)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C", 6)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "C-", 5)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "D", 3)
+#df$gradediffeq<-replace(df$gradediffeq, df$gradediffeq == "F", 0)
 
 #Difficulty (More = 1, Less = -1, else = 0, keep NA as NA):
 df$difficulty<-replace(df$difficulty, df$difficulty == "more than other courses", 1)
 df$difficulty<-replace(df$difficulty, df$difficulty == "about the same as other courses", 0)
 df$difficulty<-replace(df$difficulty, df$difficulty == "less than other courses", -1)
 
-# Format as numeric to help excel export:
-for (x in 2:18) {
+# Format as numeric to help excel export (calc3):
+#for (x in 2:18) {
+#  df[x] <- as.numeric(unlist(df[x]))
+#}
+# Format as numeric to help excel export (diffeq):
+for (x in 2:16) {
   df[x] <- as.numeric(unlist(df[x]))
 }
 
@@ -118,7 +121,7 @@ for (x in 2:18) {
 # Write out as an xlsx file
 # Change the output name depending on the file. 
 library(openxlsx)
-write.xlsx(df, "MA231 TEN AM FALL 2022.xlsx")
+write.xlsx(df, "MA232 TWELVE PM SPRING 2024.xlsx")
 
 
 
